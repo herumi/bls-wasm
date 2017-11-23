7182b97bd7b51a3ea9c87528c4b3c2f9ad2b3439
MITSUNARI Shigeo <herumi@nifty.com>
2017-11-23 17:26:35 +0900

    remove capi functions

--- test.js
+++ test.js
@@ -3,15 +3,35 @@ const assert = require('assert')
 
 bls.init()
   .then(() => {
-    const order = bls.capi.blsGetCurveOrder()
-    console.log('order=' + order)
+    console.log('curve order=' + bls.getCurveOrder())
+    serializeTest()
     signatureTest()
-    pairingTest()
     miscTest()
     shareTest()
     console.log('all ok')
     benchAll()
   })
+
+function serializeSubTest(t, cstr) {
+  const s = t.toHexStr()
+  const t2 = new cstr()
+  t2.fromHexStr(s)
+  assert.deepEqual(t.serialize(), t2.serialize())
+}
+
+function serializeTest() {
+  const sec = new bls.SecretKey()
+  sec.setByCSPRNG()
+  serializeSubTest(sec, bls.SecretKey)
+  const pub = sec.getPublicKey()
+  serializeSubTest(pub, bls.PublicKey)
+  const msg = 'abc'
+  const sig = sec.sign(msg)
+  serializeSubTest(sig, bls.Signature)
+  const id = new bls.Id()
+  id.setStr('12345')
+  serializeSubTest(id, bls.Id)
+}
 
 function signatureTest() {
   const sec = new bls.SecretKey()
@@ -30,44 +50,6 @@ function signatureTest() {
   assert(pub.verify(sig, msg))
 }
 
-function pairingTest() {
-  const capi = bls.capi
-  const a = capi.mclBnFr_malloc()
-  const b = capi.mclBnFr_malloc()
-  const ab = capi.mclBnFr_malloc()
-  const P = capi.mclBnG1_malloc()
-  const aP = capi.mclBnG1_malloc()
-  const Q = capi.mclBnG2_malloc()
-  const bQ = capi.mclBnG2_malloc()
-  const e1 = capi.mclBnGT_malloc()
-  const e2 = capi.mclBnGT_malloc()
-
-  capi.mclBnFr_setStr(a, '123')
-  capi.mclBnFr_setStr(b, '456')
-  capi.mclBnFr_mul(ab, a, b)
-  assert.equal(capi.mclBnFr_getStr(ab), 123 * 456)
-
-  capi.mclBnG1_hashAndMapTo(P, 'aaa')
-  capi.mclBnG2_hashAndMapTo(Q, 'bbb')
-  capi.mclBnG1_mul(aP, P, a)
-  capi.mclBnG2_mul(bQ, Q, b)
-
-  capi.mclBn_pairing(e1, P, Q);
-  capi.mclBn_pairing(e2, aP, bQ);
-  capi.mclBnGT_pow(e1, e1, ab)
-  assert(capi.mclBnGT_isEqual(e1, e2), 'e(aP, bQ) == e(P, Q)^ab')
-
-  capi.mcl_free(e2)
-  capi.mcl_free(e1)
-  capi.mcl_free(bQ)
-  capi.mcl_free(Q)
-  capi.mcl_free(aP)
-  capi.mcl_free(P)
-  capi.mcl_free(ab)
-  capi.mcl_free(b)
-  capi.mcl_free(a)
-}
-
 function bench(label, count, func) {
   const start = Date.now()
   for (let i = 0; i < count; i++) {
@@ -78,59 +60,17 @@ function bench(label, count, func) {
   console.log(label + ' ' + t)
 }
 
-function benchPairing() {
-  const capi = bls.capi
-  const a = capi.mclBnFr_malloc()
-  const P = capi.mclBnG1_malloc()
-  const Q = capi.mclBnG2_malloc()
-  const e = capi.mclBnGT_malloc()
-
-  const msg = 'hello wasm'
-
-  capi.mclBnFr_setByCSPRNG(a)
-  capi.mclBnG1_hashAndMapTo(P, 'abc')
-  capi.mclBnG2_hashAndMapTo(Q, 'abc')
-  bench('time_pairing', 50, () => capi.mclBn_pairing(e, P, Q))
-  bench('time_g1mul', 50, () => capi.mclBnG1_mulCT(P, P, a))
-  bench('time_g2mul', 50, () => capi.mclBnG2_mulCT(Q, Q, a))
-  bench('time_mapToG1', 50, () => capi.mclBnG1_hashAndMapTo(P, msg))
-
-  capi.mcl_free(e)
-  capi.mcl_free(Q)
-  capi.mcl_free(P)
-
-  const sec = new bls.SecretKey()
-  bench('time_setByCSPRNG', 50, () => sec.setByCSPRNG())
-}
-
 function benchBls() {
   const msg = 'hello wasm'
-  {
-    const capi = bls.capi
-    const sec = capi.blsSecretKey_malloc()
-    const pub = capi.blsPublicKey_malloc()
-    const sig = capi.blsSignature_malloc()
-
-    capi.blsSecretKeySetByCSPRNG(sec)
-    bench('time_sign', 50, () => capi.blsSign(sig, sec, msg))
-    bench('time_verify', 50, () => capi.blsVerify(sig, pub, msg))
-
-    capi.bls_free(sec)
-    capi.bls_free(pub)
-    capi.bls_free(sig)
-  }
-  {
-    const sec = new bls.SecretKey()
-    sec.setByCSPRNG()
-    const pub = sec.getPublicKey()
-    bench('time_sign_class', 50, () => sec.sign(msg))
-    const sig = sec.sign(msg)
-    bench('time_verify_class', 50, () => pub.verify(sig, msg))
-  }
+  const sec = new bls.SecretKey()
+  sec.setByCSPRNG()
+  const pub = sec.getPublicKey()
+  bench('time_sign_class', 50, () => sec.sign(msg))
+  const sig = sec.sign(msg)
+  bench('time_verify_class', 50, () => pub.verify(sig, msg))
 }
 
 function benchAll() {
-  benchPairing()
   benchBls()
 }
 
@@ -182,11 +122,11 @@ function shareTest()
     setup master secret key
   */
   for (let i = 0; i < k; i++) {
-    let sk = new bls.SecretKey()
+    const sk = new bls.SecretKey()
     sk.setByCSPRNG()
     msk.push(sk)
 
-    let pk = sk.getPublicKey()
+    const pk = sk.getPublicKey()
     mpk.push(pk)
   }
   const secStr = msk[0].toHexStr()
@@ -238,7 +178,7 @@ function shareTest()
     sec.recover(subSecVec, subIdVec)
     pub.recover(subPubVec, subIdVec)
     sig.recover(subSigVec, subIdVec)
-    let s = sec.toHexStr()
+    const s = sec.toHexStr()
     assert(sec.toHexStr(), secStr)
     assert(pub.toHexStr(), pubStr)
     assert(sig.toHexStr(), sigStr)
