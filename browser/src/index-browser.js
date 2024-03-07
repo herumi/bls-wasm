0fc5da2f6247013f133e81bcd2a917df338e143e
MITSUNARI Shigeo <herumi@nifty.com>
2024-03-07 11:46:46 +0900

    [browser] use src/index.js

--- browser/src/index-browser.js
+++
@@ -1,9 +0,0 @@
-const createModule = require('../../src/bls_c.js')
-const blsSetupFactory = require('../../src/bls.js')
-const crypto = self.crypto
-
-const getRandomValues = x => crypto.getRandomValues(x)
-const bls = blsSetupFactory(createModule, getRandomValues)
-
-module.exports = bls
-
